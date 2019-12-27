7dc8a4dad9137c50541bd6336f6f18854f0ff3a9
walneysilva <walneyjsilva@gmail.com>
2019-12-27 18:35:13 -0300

    Segundo commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("AddTech!");
+            Console.WriteLine("AddTech!!!");
             Console.ReadKey();
                 //teste 
         }
